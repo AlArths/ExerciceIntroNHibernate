ab024ab316d126efa9cac71332f742a762155e5b
bpesquet <bpesquet@gmail.com>
2018-10-09 13:45:42 +0200

    Initial commit

---
+++ Domain/Prospect.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain
+{
+    /// <summary>
+    /// POCO class representing a propect (future client)
+    /// </summary>
+    public class Prospect
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public DateTime LastContact { get; set; }
+        public string Notes { get; set; }
+
+        public Prospect(string lastName, string firstName, string email, DateTime lastContact, string notes)
+            : this(lastName, firstName, email, lastContact)
+        {
+            Notes = notes;
+        }
+        
+        public Prospect(string lastName, string firstName, string email, DateTime lastContact)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Email = email;
+            LastContact = lastContact;
+        }
+
+        public override string ToString()
+        {
+            return LastName + " " + FirstName;
+        }
+    }
+}
